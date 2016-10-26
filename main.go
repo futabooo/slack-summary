b4e31f0a12e6a10f8fcfa0a0191d08ebb8adae50
futabooo <mail@futabooo.com>
2016-10-26 20:12:00 +0900

    hello review dog

--- main.go
+++ main.go
@@ -2,9 +2,18 @@ package main
 
 import (
 	"os"
+	"fmt"
 )
+
+var vetError = fmt.Sprintf("", 1, 2, 3)
 
 func main() {
 	c := &CLI{OutStream: os.Stdout, ErrStream: os.Stderr}
 	os.Exit(c.Run(os.Args))
 }
+
+func Hoge(){
+	
+}
+
+var hoge = 1
